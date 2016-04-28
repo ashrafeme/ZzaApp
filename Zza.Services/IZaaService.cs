f36ab09253dcda7a1b4b75f219459fc1e4398c8d
ashrafeme <ashraf_eme@hotmail.com>
2016-04-28 02:03:29 -0500

    Initial commit

---
+++ Zza.Services/IZaaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace Zza.Services
+{
+    [ServiceContract]
+    public interface IZaaService
+    {
+        [OperationContract]
+        List<Product> Getproducts();
+        [OperationContract]
+        List<Customer> GetCustomers();
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
---
+++ Zza.Services/ZzaService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace Zza.Services
+{
+    public class ZzaService : IZaaService
+    {
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> Getproducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubmitOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
